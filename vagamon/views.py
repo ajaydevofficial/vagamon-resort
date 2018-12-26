ce0946a082add23b86c53d0714a6a69f1a8c9955
unknown <ajaydevofficial@gmail.com>
2018-12-26 19:28:26 +0530

    Initial Commit

---
+++ vagamon/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render,redirect
+
+def home_page(request):
+    context = {}
+    return render(request,"index.html",context)
+
+def tour_page(request):
+    context = {}
+    return render(request,"tour.html",context)
+
+def book_page(request):
+    context = {}
+    return render(request,"book.html",context)
