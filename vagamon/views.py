cf47fc1e0464adfd512b972af6cf44631d0c74c3
unknown <ajaydevofficial@gmail.com>
2019-01-15 15:48:43 +0530

    Order Checkout

--- vagamon/views.py
+++ vagamon/views.py
@@ -96,6 +96,8 @@ def checkout_page(request):
         inDate = datetime.datetime.strptime(request.session['inDate'], "%Y-%m-%d").date()
         outDate = datetime.datetime.strptime(request.session['outDate'], "%Y-%m-%d").date()
         day = (outDate-inDate).days
+        if day ==0:
+            day =1
         amount = ind*day*base
         amount_view = "Pay Rs."+str(amount)
         context = {"amount_view":amount_view,"inDate":inDate,"outDate":outDate,"ind":ind,"package":package,"Break" : False,"amount":amount}
