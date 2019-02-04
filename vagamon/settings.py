80723b38c4ae81ec2f07f232e4767e78c381ca93
unknown <ajaydevofficial@gmail.com>
2019-02-04 14:38:25 +0530

    Heroku Final

--- vagamon/settings.py
+++ vagamon/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -125,10 +125,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static_cdn"),
-    os.path.join(BASE_DIR, "static_cdn","font"),
-    os.path.join(BASE_DIR, "static_cdn","images"),
-    os.path.join(BASE_DIR, "static_cdn","css"),
+    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "static","font"),
+    os.path.join(BASE_DIR, "static","images"),
+    os.path.join(BASE_DIR, "static","css"),
 ]
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
