a1bc12755201bb32c10fd98dc6d9acd97799966b
unknown <ajaydevofficial@gmail.com>
2019-02-04 15:59:44 +0530

    Heroku Modified

--- vagamon/settings.py
+++ vagamon/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -130,7 +130,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static","images"),
     os.path.join(BASE_DIR, "static","css"),
 ]
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
 
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
